571d0d81f2b574403de4d7e88c122ca8070245b6
TheGreatFellow <shreyas42000@gmail.com>
2021-12-18 20:18:05 +0530

    added game characters and stats

--- contracts/myEpicGame.sol
+++ contracts/myEpicGame.sol
@@ -5,7 +5,52 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE.");
+    //Characte attributes
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string characterName;
+        string imageURL;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+        string origin;
+        string specialAttack;
+        uint256 level;
+    }
+
+    //default data for characters
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURLs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg,
+        string[] memory originPlaces,
+        string[] memory specialAttacks
+    ) {
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    characterName: characterNames[i],
+                    imageURL: characterImageURLs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i],
+                    origin: originPlaces[i],
+                    specialAttack: specialAttacks[i],
+                    level: 1
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.characterName,
+                c.hp,
+                c.imageURL
+            );
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,26 @@ const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory(
         'MyEpicGame'
     )
-    const gameContract = await gameContractFactory.deploy()
+    const gameContract = await gameContractFactory.deploy(
+        ['Caitlyn', 'Jinx', 'Vander', 'Jayce', 'Vi'], //names
+        [
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Caitlyn_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Jinx_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Vander_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Jayce_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Vi_Vertical_4x5_RGB.jpg',
+        ], //images
+        [1000, 700, 1500, 1260, 1100], //hp
+        [175, 250, 120, 130, 160], //attackDamage
+        ['Piltover', 'Zaun', 'Undercity', 'Piltover', 'Zaun'], //originPlaces
+        [
+            "Piltover's Peacemaker",
+            'Flame Chompers',
+            'Flashbinder',
+            'Mercury Hammer',
+            'Golem Gauntlet',
+        ] //specialAttack
+    )
     await gameContract.deployed()
     console.log('Contract is deployed to: ', gameContract.address)
 }
