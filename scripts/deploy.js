272714b71884fb37f016c3c0ce971a47982661fc
TheGreatFellow <shreyas42000@gmail.com>
2021-12-19 11:18:55 +0530

    deploy nft on the marketplace

---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory(
+        'MyEpicGame'
+    )
+    const gameContract = await gameContractFactory.deploy(
+        ['Caitlyn', 'Jinx', 'Vander', 'Jayce', 'Vi'], //names
+        [
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Caitlyn_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Jinx_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Vander_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Jayce_Vertical_4x5_RGB.jpg',
+            'https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/09/EN-US_ARCANE_Character_Vi_Vertical_4x5_RGB.jpg',
+        ], //images
+        [1000, 700, 1500, 1260, 1100], //hp
+        [175, 250, 120, 130, 160], //attackDamage
+        ['Piltover', 'Zaun', 'Undercity', 'Piltover', 'Zaun'], //originPlaces
+        [
+            "Piltover's Peacemaker",
+            'Flame Chompers',
+            'Flashbinder',
+            'Mercury Hammer',
+            'Golem Gauntlet',
+        ] //specialAttack
+    )
+    await gameContract.deployed()
+    console.log('Contract deployed to:', gameContract.address)
+
+    let txn
+    txn = await gameContract.mintCharacterNFT(0)
+    await txn.wait()
+    console.log('Minted NFT #1')
+
+    console.log('Done deploying and minting!')
+}
+
+const runMain = async () => {
+    try {
+        await main()
+        process.exit(0)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+runMain()
